d3675e246637ae1ef22baad7a6c4e41062a0d5ed
balithakur <107024128+balithakur@users.noreply.github.com>
2022-06-22 13:29:09 +0530

    test

--- first/settings.py
+++ first/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 #added manually
 #STATICFILES_DIRS=[
- #  os.path.join(BASE_DIR ,"static"),
+#   os.path.join(BASE_DIR ,"static"),
 #]
 STATIC_ROOT = BASE_DIR/ "static"
 
