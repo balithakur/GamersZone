700717ae3c32a95b130fb979793943400c493a60
bali <balisc04@gmail.com>
2022-07-29 13:03:01 +0530

    test

--- static/firstpage.js
+++
@@ -1,8 +0,0 @@
-let homebtn = document.getElementById("homebtn")
-let exitbtn = document.getElementById("exitbtn")
-homebtn.onclick = () => {
-    location.href = "home"
-}
-exitbtn.onclick = () => {
-    location.href ="https://www.google.com/"
-}
