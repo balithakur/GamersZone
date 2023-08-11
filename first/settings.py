7085fe9fccb4f95986fb83c27351c21809f5f9ad
bali singh <balisc04@gmail.com>
2023-08-11 14:15:30 +0530

    add vercel

--- first/settings.py
+++ first/settings.py
@@ -129,10 +129,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 #added manually
-#STATICFILES_DIRS = [
-#   os.path.join(BASE_DIR, "static")
-#]
-STATIC_ROOT = BASE_DIR/ "static"
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static")
+]
+#STATIC_ROOT = BASE_DIR/ "static"
 
 
 
