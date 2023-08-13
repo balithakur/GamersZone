506892a471c03aed6a68b20fdb489ceeef902045
bali singh <balisc04@gmail.com>
2023-08-13 13:15:47 +0530

    c

--- first/settings.py
+++ first/settings.py
@@ -130,10 +130,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 #added manually
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static")
-]
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 #STATIC_ROOT = BASE_DIR/ "static"
 
--- first/urls.py
+++ first/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('firstapp.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
